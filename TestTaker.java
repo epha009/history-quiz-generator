860aad311ba73db1353fb89cf9561882dc5d50c7
Edward Phan <ephan19@yahoo.com>
2018-03-13 22:41:56 -0700

    Added javaDoc to TestTaker

--- TestTaker.java
+++ TestTaker.java
@@ -1,6 +1,31 @@
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Edward Phan, Tommy Vo Tran
+ * 
+ */
+/**
+ * Class TestTaker - creates an object for each student that takes the test
+ */
 public class TestTaker {
 	private String name;
 	private int score;
+
+	/**
+	 * default constructor
+	 */
+	public TestTaker() {
+		
+	}
+	
+	/**
+	 * 
+	 * @param testTakerName - the test takers name
+	 * @param points - amount of points test taker scored
+	 */
+	public TestTaker(String testTakerName, int points) {
+		name = testTakerName;
+		score = points;
+	}
 }

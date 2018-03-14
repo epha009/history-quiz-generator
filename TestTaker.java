f171511627003a97f5729497a1a53022ff7ec485
Edward Phan <ephan19@yahoo.com>
2018-03-14 16:30:55 -0700

    Added getter and setter methods for instance variables

--- TestTaker.java
+++ TestTaker.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * 
  */
 /**
- * Class TestTaker - creates an object for each student that takes the test
+ * Class TestTaker - creates an object for each person that takes the test
  */
 public class TestTaker {
 	private String name;
@@ -26,6 +26,38 @@ public class TestTaker {
 	 */
 	public TestTaker(String testTakerName, int points) {
 		name = testTakerName;
+		score = points;
+	}
+
+	/**
+	 * method getName - getter method for the name variable
+	 * @return - TestTaker's name
+	 */
+	public String getName(){
+		return name;
+	}
+
+	/**
+	 * method getScore - getter method for the score variable
+	 * @return - TestTaker's score
+	 */
+	public int getScore(){
+		return score;
+	}
+
+	/**
+	 * method setName - setter method to set the name of the TestTaker
+	 * @param takerName - what the name will be changed to
+	 */
+	public void setName(String takerName){
+		name = takerName;
+	}
+
+	/**
+	 * method setScore - setter method to set the score of the TestTaker
+	 * @param points - what the points will be changed to
+	 */
+	public void setScore(int points){
 		score = points;
 	}
 }
